f914ce60faa4fcbe3472c7cc9f401db98548c5c9
Mohankumar <91587795+Agathokakological7@users.noreply.github.com>
2023-06-06 09:35:38 +0530

    Create Rat In a Maze All Paths.java

---
+++ Backtracking/Rat In a Maze All Paths.java
@@ -0,0 +1,52 @@
+public class Solution{   
+    
+    
+    static void printsolution(int[][] solution, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                System.out.print(solution[i][j] + " ");
+            }
+        }
+    }
+    static void solveMaze(int maze[][], int solution[][], int x, int y, int n)
+    {
+        if (x == n - 1 && y == n - 1)
+        {
+            solution[x][y] = 1;
+            printsolution(solution, n);
+            System.out.println();
+            return;
+        }
+        if (x > n - 1 || x < 0 || y > n - 1 || y < 0)
+        {
+            return;
+        }
+        if (x > n - 1 || x < 0 || y > n - 1 || y < 0 || maze[x][y] == 0 || solution[x][y] == 1)
+        {
+            return;
+        }
+        solution[x][y] = 1;
+        solveMaze(maze, solution, x - 1, y, n);
+        solveMaze(maze, solution, x + 1, y, n);
+        solveMaze(maze, solution, x, y - 1, n);
+        solveMaze(maze, solution, x, y + 1, n);
+        solution[x][y] = 0;
+    }
+    
+    static void ratInAMaze(int maze[][], int n)
+    {
+        int[][] solution = new int[20][20];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                solution[i][j] = 0;
+            }
+        }
+        solveMaze(maze, solution, 0, 0, n);
+    }
+
+}
