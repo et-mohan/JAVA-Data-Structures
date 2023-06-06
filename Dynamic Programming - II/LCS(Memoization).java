fd3823a2ef5bceed3cadf508ae63bb9a4f4b2b8e
Mohankumar <91587795+Agathokakological7@users.noreply.github.com>
2023-06-06 10:44:53 +0530

    Create LCS(Memoization).java

---
+++ Dynamic Programming - II/LCS(Memoization).java
@@ -0,0 +1,81 @@
+public class LCS {
+
+public static int Icsdp(String str1, String str2, int i, int j, int[][]dp) {
+if(i==str1. length() || j==str2. length()) {
+return 0;
+}
+int myAns; 
+if(str1.charAt(i) == str2.charAt(j)) {
+int smallAns; 
+if(dp[i+1][j+1] == -1) {
+smallAns = lcs(str1,str2, i+1, j+1);
+dp[i+1][j+1] = smal LAns;
+}else {
+smallAns = dp[i+1] [j+1];
+}
+myAns = 1 + smallAns;
+
+}else {
+int ans1, ans2;
+
+if(dp[i+1][j] == -1) {
+ans1 = Ics(str1, str2, i+1, j);
+dp[i+1][j] = ans1;
+}else{
+ans1 = dp[i+1] [j];
+}
+
+if(dp[i][j+1] == -1) {
+ans2 = lcs(str1, str2, i, j+1);
+dp[i][j+1] = ans2;
+}else{
+ans2 = dp[i][j+1];
+}
+ myAns = Math.max(ans1, ans2);
+}
+return myAns;
+}
+
+public static int lcs (String str1, String str2, int i, int i) {
+
+if(i==str1.length() ||  j==str2. length()) {
+return 0;
+}
+
+int myAns;
+if(str1.charAt(i) == str2.charAt(j)) {
+
+
+int smallAns = lcs(str1,str2, i+1, j+1);
+myAns = 1 + smallAns;
+
+}else{
+
+int ans1 = lcs(str1, str2, i+1,j);
+int ans2 = Ics(str1, str2, i, j+1);
+myAns = Math.max (ans1, ans2);
+
+}
+
+return myAns;
+}
+
+
+
+public static void main(String [] args) {
+// TODO Auto-generated method stub
+
+String str1 = "bedgmc";
+String str2 = "abdfglc";
+int[][] dp= new int[str1.length()+1][str2.length()+1];
+for(int i=0;i<dp.length;i++){
+for(int j=0;j<dp[0].length;j++){
+dp[i][j]=-1;
+}
+}
+int ans = lcsdp(str1, str2,0,0);
+System.out.println(ans);
+
+}
+
+}
