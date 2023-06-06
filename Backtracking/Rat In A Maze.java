a95f4b5718c8af8f89d5c0ee61de542945b499e6
Mohankumar <91587795+Agathokakological7@users.noreply.github.com>
2023-06-06 09:25:03 +0530

    Update Rat In A Maze.java

--- Backtracking/Rat In A Maze.java
+++ Backtracking/Rat In A Maze.java
@@ -1,41 +1,46 @@
-
 public class Solution {
+	
 
 	public static boolean ratInAMaze(int maze[][]){
-
 		int n = maze.length;
-        int path[][] = new int[n][n];
-         return solveMaze(maze, 0, 0, path);
+		int path[][] = new int[n][n];
+		return solveMaze(maze, 0, 0, path);
 	}
-       public static boolean solveMaze(int[][] maze, int i, int j, int[][] path) {
-//        Checking if the current cell which we are going to traverse is a valid cell or not?
-        if (i < 0 || i >= maze.length || j < 0 || j >= maze.length || maze[i][j] == 0 || path[i][j] == 1) {
-            return false;
-        }
-//        Including the current cell to be in path[i][j]
-        path[i][j] = 1;
-//        Checking if the current cell is destination cell or not
-        if (i == maze.length - 1 && j == maze.length - 1) {
-//            printing the path before returning
-            return true;
-        }
-//        Explore further the maze in all direction
-//        Top Direction
-        if (solveMaze(maze, i - 1, j, path)) {
-            return true;
-        }
-//        Right Direction
-        if (solveMaze(maze, i, j + 1, path)) {
-            return true;
-        }
-//        Bottom Direction
-        if (solveMaze(maze, i + 1, j, path)) {
-            return true;
-        }
-//        Left Direction
-        if (solveMaze(maze, i, j - 1, path)) {
-            return true;
-        }
-        return false;
-    }
+
+	public static boolean solveMaze(int maze[][], int i, int j, int path[][]){
+		int n = maze.length;
+		// Check if i,j cell is valid or not
+		if(i  < 0 || i >= n || j < 0 || j >= n  || maze[i][j] == 0 
+			|| 	path[i][j] == 1){
+			return false;
+		}
+		// Include the cell in current path
+		path[i][j] = 1;
+		// Destination cell
+		if(i == n - 1 && j == n - 1){	
+			path[i][j] = 0;		
+			return true;
+		}
+		
+		// Explore further in all directions
+		boolean pathPossible = false;	
+		// top
+		if(solveMaze(maze, i - 1, j, path)){
+			pathPossible = true;
+		}
+		// right 
+		else if(solveMaze(maze, i, j + 1, path)){
+			pathPossible = true;
+		}
+		// Down
+		else if(solveMaze(maze, i + 1 , j, path)){
+			pathPossible = true;
+		}
+		// Left
+		else if(solveMaze(maze, i, j - 1, path)){
+			pathPossible = true;
+		}		
+		return pathPossible;
+	}
+
 }
